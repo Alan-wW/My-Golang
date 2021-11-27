762af304ecc36128afb740343651125b446ebcff
Alan_ <alanwilson@163.com>
2021-11-27 19:09:19 +0800

    lecture03：流程控制

---
+++ 函数/函数.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		函数
+		小写字母开头的函数只在本包内可见，大写字母开头的函数才 能被其他包使用。
+	*/
+	b := add(1, 2)
+	fmt.Println(b)
+
+	fmt.Println(addArgs(1, 2, 3, 4, 5))
+
+	fmt.Println(moreReturnVal(1, 2))
+}
+
+func add(x int, y int) int {
+	return x + y
+}
+
+//不定参数 ...type实质是一个切片
+func addArgs(args ...int) int {
+	sum := 0
+	for _, val := range args {
+		sum += val
+	}
+	return sum
+}
+
+//任意类型的不定参数
+func functionInterface(args ...interface{}) {
+	for _, arg := range args {
+		switch arg.(type) {
+		case int:
+			fmt.Println("int")
+		case string:
+			fmt.Println("string")
+		}
+	}
+}
+
+//多返回值,如果不想使用某个返回值，可以用'_'替换
+func moreReturnVal(x int, y int) (int, int) {
+	x, y = y, x
+	return x, y
+}
